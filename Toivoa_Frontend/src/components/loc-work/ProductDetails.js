91fd0a191c4f376f90efde95ac058133a87f6593
locphuocdang <dangloc2407@gmail.com>
2024-09-12 17:08:13 +0300

    modify image to correct path adn folder

--- Toivoa_Frontend/src/components/loc-work/ProductDetails.js
+++ Toivoa_Frontend/src/components/loc-work/ProductDetails.js
@@ -1,10 +1,12 @@
 // src/components/ProductPage.js
 import React, { useState } from "react";
 import "./ProductDetails.css"; // Ensure your CSS file is linked here
-import ps5_main from "../../images/ps5.png";
-import ps5_1 from "../images/ps5_controller1.png";
-import ps5_2 from "../images/ps5_controller2.jpg";
-import ps5_3 from "../images/ps5_controller3.png";
+import ps5_main from "../../images/ps5_controller_main.png";
+import ps5_1 from "../../images/ps5_controller1.png";
+import ps5_2 from "../../images/ps5_controller2.jpg";
+import ps5_3 from "../../images/ps5_controller3.png";
+import ps5_4 from "../../images/ps5_controller4.webp";
+
 
 const productData = {
   id: 1,
@@ -12,7 +14,7 @@ const productData = {
   reviews: 80,
   price: 79.99,
   description: "High-quality sneakers perfect for running and casual wear.",
-  images: [ps5_main, ps5_1, ps5_2, ps5_3],
+  images: [ps5_main, ps5_1, ps5_2, ps5_3, ps5_4],
   sizes: ["S", "M", "L", "XL"],
   isInStock: true,
   colors: [
--- Toivoa_Frontend/src/components/loc-work/data.js
+++ Toivoa_Frontend/src/components/loc-work/data.js
@@ -1,8 +1,8 @@
 import ps5_main from "../images/ps5_controller_main.png";
-import ps5_1 from "../images/ps5_controller1.png";
-import ps5_2 from "../images/ps5_controller2.jpg";
-import ps5_3 from "../images/ps5_controller3.png";
-import ps5_4 from "../images/ps5_controller4.webp";
+import ps5_1 from "../../images/ps5_controller1.png";
+import ps5_2 from "../../images/ps5_controller2.jpg";
+import ps5_3 from "../../images/ps5_controller3.png";
+import ps5_4 from "../../images/ps5_controller4.webp";
 
 export const productData = {
   id: 1,
@@ -10,7 +10,7 @@ export const productData = {
   reviews: 80,
   price: 79.99,
   description: "High-quality sneakers perfect for running and casual wear.",
-  images: [ps5_main, ps5_1, ps5_2, ps5_3],
+  images: [ps5_main, ps5_1, ps5_2, ps5_3, ps5_4],
   sizes: ["S", "M", "L", "XL"],
   isInStock: true
 };
