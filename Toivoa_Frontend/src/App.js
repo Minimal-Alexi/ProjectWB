5e4118caf6d49661ca6973a5e00a3ed033797c61
Minimal-Alexi <alexpop@metropolia.fi>
2024-09-12 16:42:45 +0300

    Created server routing. Please use this for your code.

--- Toivoa_Frontend/src/App.js
+++ Toivoa_Frontend/src/App.js
@@ -1,11 +1,19 @@
-import ProductDetails from './components/loc-work/ProductDetails'
+//import ProductDetails from './components/loc-work/ProductDetails'
+import UserSettingsPage from './components/alex-work/UserSettingsEdit'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <ProductDetails />
-    </div>
+    <BrowserRouter>
+        <Navigation />
+          <Routes>
+            {/* <Route path="/Product" element={<Product />} /> */}
+            <Route path="/Settings" element={<UserSettingsPage />} />
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+    </BrowserRouter>
   );
 }
 
