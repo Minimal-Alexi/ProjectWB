0cf9928d16c69f60f957a8835500acd9e60ca6aa
Minimal-Alexi <alexpop@metropolia.fi>
2024-09-13 11:53:17 +0300

    Changed the user model to the DB model. User controller functions are asynchronous. Server will try multiple times to connect to the database before giving up.

--- Toivoa_Backend/Controller/userController.js
+++ Toivoa_Backend/Controller/userController.js
@@ -1,76 +1,99 @@
 const Users = require('../Models/userModel');
+const mongoose = require("mongoose");
 
 //GET /users
-const getAllUsers = (req,res) =>
-    {
-        res.json(Users.getAll());
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await Users.find({}).sort({ createdAt: -1 });
+        res.status(200).json(users);
     }
+    catch (error) {
+        res.status(500).json({ message: "Failed to retrieve users." });
+    }
+};
 
 //GET /users/:userID
 
-const getUserbyID = (req,res) =>
-    {
-        const userID = req.params.userID;
-        const user = Users.findOnebyID(userID);
-        if(user)
-            {
-                res.json(user)
-            }
-        else
-        {
-            res.status(404).json({message:"User not found."});
+const getUserbyID = async (req, res) => {
+    const userID = req.params.userID;
+    if (!mongoose.Types.ObjectId.isValid(userID)) {
+        return res.status(400).json({ message: "Invalid userID" })
+    }
+    try {
+        const user = await Users.findById(userID);
+        if (user) {
+            res.status(200).json(user);
+        }
+        else {
+            res.status(404).json({ message: "User not found." });
         }
     }
+    catch (error) {
+        res.status(500).json({ message: "Failed to retrieve user." });
+    }
+}
 
 //POST /users
 
-const createUser = (req,res) =>
+const createUser = async (req, res) => {
+    try
     {
-        console.log(req.body);
-        const user = Users.addOne({...req.body})
-        if(user)
-            {
-                res.status(201).json(user);
-            }
-        else
-        {
-            res.status(500).json({message:"User could not be created."});
-        }
+        //process passwords first before anything else! TO BE DONE
+        const newUser = await Users.create({...req.body})
+        res.status(201).json(newCar);
     }
+    catch (error) {
+        res.status(400).json({ message: "Failed to create user", error: error.message });
+    }
+}
 
 //PUT /users/:userID
 
-const updateUser = (req,res) =>
-    {
-        const userID = req.params.userID;
-        updatedUser = Users.updateOnebyID(userID);
-        if(updatedUser)
-            {
-                res.json(updatedUser);
-            }
-        else
-        {
-            res.status(404).json({message:"User not found."});
-        }
+const updateUser = async (req, res) => {
+    const userID = req.params.userID;
+    if (!mongoose.Types.ObjectId.isValid(userID)) {
+        return res.status(400).json({ message: "Invalid userID" })
     }
+    try {
+            const updatedUser = await Users.findOneAndUpdate(
+                {_id:userID},
+                {...req.body},
+                {new: true,overwrite:true},
+            )
+            if (updatedUser) {
+                res.status(200).json(updatedUser);
+            }
+            else {
+                res.status(404).json({ message: "User not found." });
+            }
+        }
+    catch (error) {
+            res.status(500).json({ message: "Failed to update user." });
+        }
+}
 
 //DELETE /users/:userID
 
-const deleteUser = (req,res) =>
-    {
-        const userID = req.params.userID;
-        deletedUser = Users.deleteOnebyID(userID);
-        if(deletedUser)
-            {
-                res.status(204).send();
-            }
-        else
-        {
-            res.status(404).json({message:"User not found."});
+const deleteUser = async (req, res) => {
+    const userID = req.params.userID;
+    if (!mongoose.Types.ObjectId.isValid(userID)) {
+        return res.status(400).json({ message: "Invalid userID" })
+    }
+    try {
+        const deletedUser = await Users.findOneAndDelete({_id:userID})
+        if (deletedUser) {
+            res.status(200).json({message:"User deleted successfully."});
+        }
+        else {
+            res.status(404).json({ message: "User not found." });
         }
     }
+    catch (error) {
+        res.status(500).json({ message: "Failed to update user." });
+    }
+}
 
-module.exports = 
+module.exports =
 {
     getAllUsers,
     getUserbyID,
--- Toivoa_Backend/Models/userModel.js
+++ Toivoa_Backend/Models/userModel.js
@@ -1,118 +1,74 @@
 /*
-    username: "Gigel_Costel",
-    password: "LupuDacic",
-    email: "LupuDacicAuAu@yahoo.com",
-    age: 69,
-    location: Manastur,
+    userID: "ajKJE1K32LLSADmcx"
+    username: "Gigel_Costel"
+    email: "Gigel@yahoo.com"
+    password: "ASDKSLKLMXAS"
+    password_salt: "LSKDASLKXLA"
+    account_type: 0
+    country: "US"
+    location: "Alea manastur"
+    phone_number: 12938398219581958
+    age: 69
     gender: "M"
 */
 
-let userArray = [];
-let nextID = 1;
+const mongoose = require("mongoose");
 
-const getAll = () =>
-    {
-        return userArray;
-    }
+const Schema = mongoose.Schema;
 
-const addOne = (userData) =>
+const userSchema = new Schema(
     {
-        const {username,password,email,age,location,gender} = userData;
-        if (!username,!age,!password,!email,!location,!gender)
+        username:
         {
-            return false;
-        }
-        const newUser = 
+            type: String,
+            required: true,
+        },
+        email:{
+            type: String,
+            required: true,
+        },
+        password:
         {
-            id:nextID++,
-            ...userData
-        }
-        userArray.push(newUser);
-        return newUser;
-    }
-
-const findOnebyID = (id) => 
-    {
-        const numericID = Number(id)
-        const foundUser = userArray.find(user => user.id === numericID);
-        return foundUser || false;
-    }
-
-const updateOnebyID = (id,updateData) => 
-    {
-        const updateUser = findOnebyID(id);
-        if(updateUser)
-            {
-                if(updateData)
-                    {
-                        Object.assign(updateUser,updateData);
-                    }
-                return updateUser;
-            }
-        return false;
-    }
-
-const deleteOnebyID = (id) => 
-    {
-        const deleteUser = findOnebyID(id);
-        if (deleteUser)
-            {
-                userArray = userArray.filter(user => user.id!==deleteUser.id);
-                return true;
-            }
-        return false;
-    }
-
-if(require.main === module)
-    {
-        //Creating users, 2 valid 1 invalid. Test for impossibilities.
-        console.log("addOne Called:",
-            addOne(
+            type:String,
+            required:true,
+        },
+        passwordSalt:
         {
-            username: "Gigel_Costel",
-            password: "LupuDacic",
-            email: "LupuDacicAuAu@yahoo.com",
-            age: 69,
-            location: "Manastur",
-            gender: "M"
-        }))
-        console.log("addOne Called:",
-            addOne(
+            type:String,
+            required:true
+        },
+        accountType:
         {
-            username: "Minerva_Alexia",
-            password: ":3",
-            email: "Miss_Metropolia@Metropolia.fi",
-            age: 21,
-            location: "Hillapelontie",
-            gender: "F"
-        }))
-        console.log("addOne Called:",
-            addOne(
+            type:Number,
+            required:true,
+        },
+        countryCode:
         {
-            username: "Faker - Get Faked."
-        }))
+            type:String,
+            required:true,
+        },
+        location:
+        {
+            type:String,
+            required:false,
+        },
+        phoneNumber:
+        {
+            type:String,
+            required:false,
+        },
+        age:
+        {
+            type:Number,
+            required:false,
+        },
+        gender:
+        {
+            type:String,
+            required:false,
+        },
+    },
+    { timestamps: true }
+);
 
-        //Checking array. Test for impossibilities.
-        console.log("getAll Called:",getAll());
-        console.log("getOnebyId called:",findOnebyID(1));
-        console.log("getOnebyId called:",findOnebyID(3));
-
-        //Checking update. Test for impossibilities.
-        console.log("updateOnebyId called:", updateOnebyID(1,{username:"Catalin_Fatalau"}));
-        console.log("updateOnebyId called:", updateOnebyID(3,{username:"Faker - Get Faked."}));
-
-        //Checking delete. Test for impossibilities.
-        console.log("deleteOnebyId called:", deleteOnebyID(1));
-        console.log("deleteOnebyId called:", deleteOnebyID(3));
-        //Final array should only have item 2.
-        console.log("getAll Called:",getAll());
-    }
-
-    module.exports = 
-    {
-        getAll,
-        addOne,
-        findOnebyID,
-        updateOnebyID,
-        deleteOnebyID,
-    }
+module.exports = mongoose.model("User", userSchema);
