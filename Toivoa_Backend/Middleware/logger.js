0083c39c702b37a9217bff8cd20aa8a58f16f147
Minimal-Alexi <alexpop@metropolia.fi>
2024-09-13 10:51:41 +0300

    Implemented a better logger function. Created error handling API.

--- Toivoa_Backend/Middleware/logger.js
+++ Toivoa_Backend/Middleware/logger.js
@@ -1,8 +1,12 @@
-const logger = (req, res, next) => {
+const requestLogger = (req, res, next) => {
     const currentDate = new Date().toISOString();
     console.log(`[${currentDate}] ${req.method} request to ${req.url}`);
-    next(); // Pass control to the next middleware function
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
   };
   
-  module.exports = logger;
+  module.exports = requestLogger;
   
--- Toivoa_Backend/app.js
+++ Toivoa_Backend/app.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
-const port = require('./security_credentials.json').port;
+require('dotenv').config();
+const port = process.env.PORT || 4000
 
 const userRouter = require("./Router/userRouter");
 
 
 const logger = require("./Middleware/logger");
+const {unknownEndpoint,errorHandler} = require("./Middleware/errorHandling");
 
 //middleware
 app.use(express.json());
 app.use(logger)
 
+
+//routes
+
 app.use("/users",userRouter)
 
+
+// Error handling
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 // Start the server
 app.listen(port, () => {
