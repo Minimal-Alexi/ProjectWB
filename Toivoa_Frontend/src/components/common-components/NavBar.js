6c49d701c1c61ba0f62b72797645060e9613cb73
Thien Luu <ngocthien0105@gmail.com>
2024-09-14 22:32:41 +0300

    restructure App.js and Main.js, add feature complete Cart page, adjust some styles

--- Toivoa_Frontend/src/components/common-components/NavBar.js
+++ Toivoa_Frontend/src/components/common-components/NavBar.js
@@ -1,10 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import main_logo from "../../images/main_logo.png";
-import { useState, useRef, useEffect } from 'react';
-import Create from '../loginRegistryPage/Create';
-import Login from '../loginRegistryPage/Login';
-import '../loginRegistryPage/login-create.css';
-import { ShoppingCart, MagnifyingGlass, Heart, User  } from "phosphor-react";
+import { useState, useRef, useEffect } from "react";
+import Create from "../loginRegistryPage/Create";
+import Login from "../loginRegistryPage/Login";
+import "../loginRegistryPage/login-create.css";
+import { ShoppingCart, MagnifyingGlass, Heart, User } from "phosphor-react";
 
 const NavBar = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -31,23 +31,23 @@ const NavBar = () => {
   };
 
   const closeLoginAndCreate = (event) => {
-    if (
-      (ref.current && !ref.current.contains(event.target))
-    ) {
+    if (ref.current && !ref.current.contains(event.target)) {
       setShowLogin(false);
       setShowCreate(false);
     }
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', closeLoginAndCreate);
-    return () => document.removeEventListener('mousedown', closeLoginAndCreate);
+    document.addEventListener("mousedown", closeLoginAndCreate);
+    return () => document.removeEventListener("mousedown", closeLoginAndCreate);
   }, []);
 
   return (
     <nav className="navbar">
       <div className="logo-search-container">
-        <img src={main_logo} alt="Toivoa Logo" />
+        <Link to="/" className="main" aria-label="Main Page">
+          <img src={main_logo} alt="Toivoa Logo" />
+        </Link>
         <div className="search">
           <form action="#" method="get" aria-label="Search Form">
             <input
@@ -61,20 +61,32 @@ const NavBar = () => {
         </div>
       </div>
       <div className="user-options">
-        <Link to="/" className="cart" aria-label="View Cart">
-        <ShoppingCart size={32} />
+        <Link to="/cart" className="cart" aria-label="View Cart">
+          <ShoppingCart size={32} />
         </Link>
         <Link to="/" className="wishlist" aria-label="View Wishlist">
-        <Heart size={32} />
+          <Heart size={32} />
         </Link>
         <Link to="/" className="user-icon" aria-label="User">
-        <User size={32} />
+          <User size={32} />
         </Link>
-        <a onClick={clickEventCreate} className="sign-up-btn">Sign Up</a>
-        <a onClick={clickEventLogin} className="sign-in-btn">Sign In</a>
+        <a onClick={clickEventCreate} className="sign-up-btn">
+          Sign Up
+        </a>
+        <a onClick={clickEventLogin} className="sign-in-btn">
+          Sign In
+        </a>
         <div ref={ref} className="login-create">
-          {showLogin ? (<div className="popup-container"><Login switchToCreate={showC} /></div>) : null}
-          {showCreate ? (<div className="popup-container"><Create switchToLogin={showL} /></div>) : null}
+          {showLogin ? (
+            <div className="popup-container">
+              <Login switchToCreate={showC} />
+            </div>
+          ) : null}
+          {showCreate ? (
+            <div className="popup-container">
+              <Create switchToLogin={showL} />
+            </div>
+          ) : null}
         </div>
       </div>
     </nav>
--- Toivoa_Frontend/src/components/mainPage/Main.js
+++ Toivoa_Frontend/src/components/mainPage/Main.js
@@ -1,23 +1,18 @@
 import Hero from "./Hero";
-import NavBar from "../common-components/NavBar";
 import TrendingDiscover from "./TrendingDiscovery";
 import ProductCard from "../common-components/ProductCard";
 import ViewAllButton from "../common-components/ViewAll";
-import Footer from "../common-components/Footers";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ShopContextProvider } from "../shoppingCartPage/shopContext";
 
 //import './style.css';
 
 const Main = () => (
   <div className="Main">
-    <Router>
-      <NavBar />
-    </Router>
     <Hero />
     <TrendingDiscover />
     <ProductCard />
     <ViewAllButton />
-    <Footer />
   </div>
 );
 
