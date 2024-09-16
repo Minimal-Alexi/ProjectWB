b6f6805bdc667d5c3a9ed3a61e8fe56ae725e8fd
vikko <vikkov@metropolia.fi>
2024-09-16 20:15:53 +0300

    error page created

--- Toivoa_Frontend/src/App.js
+++ Toivoa_Frontend/src/App.js
@@ -1,12 +1,13 @@
 //import ProductDetails from './components/loc-work/ProductDetails'
 //import UserSettingsPage from './components/userSettings/UserSettingsEdit'
 import Main from './components/mainPage/Main';
+import ErrorPage from './components/notfound-page/ErrorPage';
 import './App.css';
 
 
 function App() {
   return (
-    <Main />
+    <ErrorPage />
   );
 }
 
