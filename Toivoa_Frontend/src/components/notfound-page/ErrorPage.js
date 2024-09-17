a1f9bf53155232ca2998d3b60fd408bc46e06ec0
vikko <vikkov@metropolia.fi>
2024-09-17 15:29:45 +0300

    Removed double footer and navbar fron errorpage

--- Toivoa_Frontend/src/components/notfound-page/ErrorPage.js
+++ Toivoa_Frontend/src/components/notfound-page/ErrorPage.js
@@ -1,13 +1,9 @@
-import NavBar from '../common-components/NavBar';
-import Footer from '../common-components/Footers';
 import NotFound from './NotFound';
 import './NotFoundStyling.css'
 
 const ErrorPage = () => (
   <div className="error-page-container">
-    <NavBar />
     <NotFound />
-    <Footer />
   </div>
 );
 
