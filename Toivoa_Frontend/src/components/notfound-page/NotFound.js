538f9ee57cbae721f0cba6680b217ff01e06fa2c
Thien Luu <ngocthien0105@gmail.com>
2024-09-18 03:19:59 +0300

    Added more products to data, added a nav link from Error Page to Main Page, added responsive styling to the Cart page

--- Toivoa_Frontend/src/components/notfound-page/NotFound.js
+++ Toivoa_Frontend/src/components/notfound-page/NotFound.js
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
+
+
 const NotFound = (openMainPage) => {
+  const navigate = useNavigate();
   return (
     <div className="notfound">
       <ul className="not-found-list-of-items">
         <li><h1>404 Not Found</h1></li>
         <li>You're searched page was not found</li>
-        <li><button>Back to home page</button></li>
+        <li><button onClick={() => navigate("/")}>Back to home page</button></li>
       </ul>
     </div>
   );
