13b02fefb92280fa896939fb765a238ea37d3c1f
Minimal-Alexi <alexpop@metropolia.fi>
2024-10-07 17:29:47 +0300

    Fixed proxy bug.

--- Toivoa_Frontend/src/hooks/useLogin.js
+++ Toivoa_Frontend/src/hooks/useLogin.js
@@ -13,7 +13,7 @@ export default function useLogin(url) {
     setError(null);
     
     try {
-      const response = await fetch(`http://localhost:4000${url}`, {
+      const response = await fetch(`${url}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(object),
